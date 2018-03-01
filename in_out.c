e55780522a30d086cfc43a8aacc76d1045a8a918
ggorg03 <gustavobagorgonio03@gmail.com>
2018-03-01 19:07:34 -0300

    apdated with the types of scientific notation

--- in_out.c
+++ in_out.c
@@ -14,9 +14,11 @@ scanf("%f", &c);
 scanf("%lf", &d);
 
 //Imprime os valores das variaveis junto a uma mensagem no prompt
-printf("Eu sou um %c do tipo character\n", a);
-printf("Eu sou um %i do tipo interiro\n", b);
-printf("Eu sou um %f do tipo float\n", c);
-printf("Eu sou um %lf do tipo double\n", d);
+printf("Eu sou um %c e meu tipo character\n", a);
+printf("Eu sou um %i e meu tipo interiro\n", b);
+printf("Eu sou um %f e meu tipo float\n", c);
+printf("Eu sou um %lf e meu tipo double\n", d);
+printf("Eu sou um %e e meu tipo notação cientifica\n", d);
+printf("Eu sou um %E e meu tipo notação cientifica\n", d);
 
 }
